c316532d7ae15c834b1039a0818dd8f1b402a656
Makoa <32736507+santarini@users.noreply.github.com>
2018-04-27 12:09:51 -1000

    Create bregRequest2.py

---
+++ bregRequest2.py
@@ -0,0 +1,32 @@
+#!Python 3
+
+import os
+import requests
+import bs4 as bs
+import csv
+import re
+
+response = requests.get('https://hbe.ehawaii.gov/documents/search.html?recordType=ALL&status=ALL&beginsWith=true&query=the')
+soup = bs.BeautifulSoup(response.text, 'lxml')
+
+#check if search term has values
+try:
+    noneText = re.search('There are no businesses for this search term.', soup.text)
+    if noneText.group(0) is not None:
+        #label as being none
+        #break
+except:
+
+#check if more than 300 results for search term
+try:
+    maxText = re.search('Displaying only the first', soup.text)
+    if maxText.group(0) is not None:
+        span = soup.find("span", {"class": "red"}).text
+        instanacesOfCharRegex = re.compile(r'\d{3}')
+        instanacesOfChar = instanacesOfCharRegex.findall(span)[1]
+        instanacesOfChar = instanacesOfChar.replace(",", "")
+        print(instanacesOfChar)
+except:
+    #pull data
+    
+
