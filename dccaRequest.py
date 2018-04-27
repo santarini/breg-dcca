1c2a19e2a6656d3d6c5df0f329905e8eca8f98a1
Makoa <32736507+santarini@users.noreply.github.com>
2018-04-27 10:51:29 -1000

    Update dccaRequest.py

--- dccaRequest.py
+++ dccaRequest.py
@@ -10,8 +10,17 @@ response = requests.get('https://hbe.ehawaii.gov/documents/search.html?beginsWit
 soup = bs.BeautifulSoup(response.text, 'lxml')
 parentTable = soup.findAll("table")[1]
 mainTable = parentTable.findAll("td")[1]
+
+#figure out page count
 span = soup.find("span", {"class": "red"}).text
-itemPerPage = re.search(r'\d{3}', span)
-instanacesOfChar = re.search(r'\d{1,3}\,\d{3}', span)
+itemPerPageRegex = re.compile(r'\d{3}')
+itemPerPage = itemPerPageRegex.findall(span)[0]
+
+instanacesOfCharRegex = re.compile(r'\d{1,3}\,\d{3}')
+instanacesOfChar = instanacesOfCharRegex.findall(span)[0]
 instanacesOfChar = instanacesOfChar.replace(",", "")
+
 pageCount = int(instanacesOfChar)/int(itemPerPage)
+
+#isolate company listings
+listingTable = mainTable.findAll("span", {"id": "table1"})
